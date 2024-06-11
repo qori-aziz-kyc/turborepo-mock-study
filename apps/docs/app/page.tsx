6d080c566154a83f8366db7b4bb105f3e1f9b484
qori-aziz-kyc <168608559+qori-aziz-kyc@users.noreply.github.com>
2024-06-11 17:33:39 +0900

    Create turborepo app

--- apps/docs/app/page.tsx
+++ apps/docs/app/page.tsx
@@ -1,107 +1,57 @@
-import { Button } from "@repo/ui/button";
-import Image from "next/image";
+'use client'
+import { useEffect } from "react";
+import * as THREE from 'three';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { OrbitControls} from 'three/addons/controls/OrbitControls.js';
+
 
 export default function Home() {
-  return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm text-center sm:text-left">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  let canvas: HTMLElement
+  useEffect(() => {
+    if (canvas) return
+    const renderer = new THREE.WebGLRenderer({
+      canvas: canvas || undefined,
+      antialias: true,
+      alpha: true
+    })
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
+    canvas = document.getElementById('canvas')!
+    const scene = new THREE.Scene();
+    scene.background = new THREE.Color( 0xbfe3dd );
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-        <Button
-          appName="docs"
-          className="mx-auto rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-        >
-          Open alert
-        </Button>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file-text.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    const loader = new GLTFLoader();
+    
+    const camera = new THREE.PerspectiveCamera( 20, window.innerWidth / window.innerHeight, 1, 100 );
+		camera.position.set( -10, 0, 10 );
+    const controls = new OrbitControls(camera, renderer.domElement);
+    controls.update();
+    
+    /// lighting ///
+
+    const light = new THREE.AmbientLight(0xffffff);
+    light.position.set(10, 10, 10);
+    scene.add(light);
+
+    loader.load('/model/adamHead/adamHeadNew.gltf', function ( gltf ) {
+      scene.add( gltf.scene )
+  }, undefined, function ( error ) {
+    console.error( error );
+  } );
+
+  // MUST HAVE TO RENDER ANIMATION EVERY SEC!!!
+  const tick = () => {
+    window.requestAnimationFrame(tick)
+    renderer.render(scene, camera)
+  }
+  tick()
+  },[])
+
+  return (
+    <>
+      <canvas id="canvas"></canvas>
+    </>
   );
 }
--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,107 +1,84 @@
+'use client';
+
+import { useRef } from 'react';
+import gsap from 'gsap';
+import { useGSAP } from '@gsap/react';
+import { ScrollTrigger } from 'gsap/dist/ScrollTrigger';
 import { Button } from "@repo/ui/button";
-import Image from "next/image";
+
+if (typeof window !== 'undefined') {
+  gsap.registerPlugin(ScrollTrigger, useGSAP);
+}
 
 export default function Home() {
-  return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm text-center sm:text-left">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const container = useRef<HTMLElement | any>();
+  const container2 = useRef<HTMLElement | any>();
+  const tl = useRef<GSAPTimeline | any>();
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
+  const toggleTimeline = () => {
+    tl.current.reversed(!tl.current.reversed());
+  };
+
+  useGSAP(
+    () => {
+      const boxes = gsap.utils.toArray('.box');
+      tl.current = gsap
+        .timeline()
+        .to(boxes[0], { x: 150, y: 80, rotation: 360 })
+        .to(boxes[1], { x: 50, y: -10, rotation: 360 })
+        .to(boxes[2], { x:-150,y: -100, rotation: -360 })
+        .to(boxes[3], { x:-50 ,y: -190, rotation: -360  })
+        .reverse();
+    },
+    { scope: container }
+  );
+
+  useGSAP(
+    () => {
+        const circles = gsap.utils.toArray('.circle')
+        circles.forEach((circle) => {
+          gsap.to(circle, {
+            x: -900,
+            y : -400,
+            scrollTrigger: {
+              trigger: circle,
+              start: 'bottom bottom',
+              end: 'top 20%',
+              scrub : true,
+            }
+          })
+        })
+    }, {scope:container2}
+  );
+
+  return (
+    <main>
+      <section className="boxes-container" ref={container}>
+        <h2>Test Site: Click button</h2>
         <Button
-          appName="web"
+          onClick = {toggleTimeline}
           className="mx-auto rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
         >
-          Open alert
+          Toggle Timeline
         </Button>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file-text.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+          {/* <button onClick={toggleTimeline}>Toggle Timeline</button> */}
+        <div className="box gradient-green-2">I</div>
+        <div className="box gradient-blue">R</div>
+        <div className="box gradient-red">Q</div>
+        <div className="box gradient-green-2">O</div>
+      </section>
+      <section className="section flex-center column">
+        <h2>Basic ScrollTrigger !</h2>
+        <p>Scroll down to start</p>
+      </section>
+      <div className="section2 flex-right column" ref={container2}>
+        <div className="circle gradient-red">circle 1</div>
+        <div className="circle gradient-blue">circle 2</div>
+        <div className="circle gradient-green-2">circle 3</div>
+        <div className="circle gradient-red">circle 4</div>
+        <div className="circle gradient-blue">circle 5</div>
+        <div className="circle gradient-green-2">circle 6</div>
+      </div>
+    </main>
   );
 }
